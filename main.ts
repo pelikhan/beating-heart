b55d8a21a3e7441a4c8c1401f1f1e84c360a7ef0
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-04-30 13:26:53 -0700

    Restore '4/30/2020, 1:25:22 PM added some cool icon'

--- main.ts
+++ main.ts
@@ -2,5 +2,4 @@ basic.forever(function () {
     basic.showIcon(IconNames.Heart)
     basic.showIcon(IconNames.SmallHeart)
     basic.showIcon(IconNames.TShirt)
-    basic.showIcon(IconNames.Asleep)
 })
